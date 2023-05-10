a896a6b5c26228a69975b4a1348973f5a8a17051
phazarik <prachurjyahazarika@gmail.com>
2023-05-11 00:24:35 +0530

    Working helloworld

--- codes/1_introduction/hello.cu
+++ codes/1_introduction/hello.cu
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
+//The following function runs in CPU.
 void helloCPU(){printf("Hello from the CPU.\n");}
 
+//The follogin function runs on GPU.
 //The addition of `__global__` signifies that this function should be launced on the GPU.
-
 __global__ void helloGPU(){printf("Hello from the GPU.\n");}
 
 int main(){
     helloCPU();
-
     //Add an execution configuration with the <<<...>>> syntax.
     //It will launch this function as a kernel on the GPU.
-
     helloGPU<<<1, 1>>>();
-
     //`cudaDeviceSynchronize` will block the CPU stream until all GPU kernels have completed.
-
     cudaDeviceSynchronize();
 }
